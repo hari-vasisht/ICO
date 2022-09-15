535283afa12b268807431947d6343413dc3d53d8
hari-vasisht <shsvasisht@gmail.com>
2022-09-15 12:10:33 +0530

    feat:Backend deploy js

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,31 +1,24 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { NFT_Contract_Address } = require("../constants");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-  const lockedAmount = hre.ethers.utils.parseEther("1");
-
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-
-  await lock.deployed();
-
+  const NFT_Contract = NFT_Contract_Address;
+  const crytpoDreamTokenContract = await ethers.getContarctFactory(
+    "CryptoDreamToken"
+  );
+  const deployedCryptoDreamTokenContract = await ethers.deploy(
+    crytpoDreamTokenContract
+  );
   console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+    "CryptoDreamToken Contaract is deployed at:",
+    deployedCryptoDreamTokenContract.address
   );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
