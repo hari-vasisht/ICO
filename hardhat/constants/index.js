452c98b0f17250ed201e2d375b92b1ae0f8f5908
hari-vasisht <shsvasisht@gmail.com>
2022-09-15 12:40:49 +0530

    fix:bug in index js

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,3 +1,3 @@
-const { NFT_Contract_Address } = "0xfc956a27034F3693f7766668Bb208b5cc5d94400";
+const NFT_Contract_Address = "0xfc956a27034F3693f7766668Bb208b5cc5d94400";
 
 module.exports = { NFT_Contract_Address };
